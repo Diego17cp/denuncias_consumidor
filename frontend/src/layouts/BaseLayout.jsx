7ee11dce7300c68f03b8a9f83319120e5b73719f
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-13 21:18:41 -0500

    fix: add background color to BaseLayout for improved visual consistency

--- frontend/src/layouts/BaseLayout.jsx
+++ frontend/src/layouts/BaseLayout.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router"
-import Header from "../components/Header"
+import { Outlet } from "react-router";
+import Header from "../components/Header";
 
 export const BaseLayout = () => {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <main className="flex-1 w-full">
-                <Header />
-                <Outlet />
-            </main>
-        </div>
-    )
-}
+	return (
+		<div className="flex flex-col min-h-screen bg-white">
+			<Header />
+			<main className="flex-1 w-full">
+				<Outlet />
+			</main>
+		</div>
+	);
+};
