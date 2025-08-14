f72e491e500aa018b7f78fc67fe9c003f66c8bd7
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-13 21:30:55 -0500

    refactor: clean up commented imports and enhance fallback loader in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,41 +2,52 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router";
 
 // Componentes
-// import { loader } from "./components/loaders/Loader";
+import { Loader } from "dialca-ui";
+import { BaseLayout } from "./layouts/BaseLayout";
+import { FormDenuncia } from "./pages/public/Form/FormDenuncia";
 
 // Paginas Admin
-const Login = lazy(() => import("./pages/admin/Login"));
-const DashboardAdmin = lazy(() => import("./pages/admin/Dashboard"));
-const AdminsHistorial = lazy(() => import("./pages/admin/AdminsHistorial"));
-const SearchAdmin = lazy(() => import("./pages/admin/AdministrarUsuarios/SearchAdmin"));
-const DenunciasRecibidas = lazy(() => import("./pages/admin/denuncias/DenunciasRecibidas"));
-const SearchDenuncia = lazy(() => import("./pages/admin/denucias/SearchDenuncia"));
-const Denuncias = lazy(() => import("./pages/admin/denucias/Denuncias"));
-const AdministrarUsuarios = lazy(() => import("./pages/admin/administrarUsuarios/AdministrarUsuarios"));
+// const Login = lazy(() => import("./pages/admin/Login"));
+// const DashboardAdmin = lazy(() => import("./pages/admin/Dashboard"));
+// const AdminsHistorial = lazy(() => import("./pages/admin/AdminsHistorial"));
+// const SearchAdmin = lazy(() =>
+// 	import("./pages/admin/AdministrarUsuarios/SearchAdmin")
+// );
+// const DenunciasRecibidas = lazy(() =>
+// 	import("./pages/admin/denuncias/DenunciasRecibidas")
+// );
+// const SearchDenuncia = lazy(() =>
+// 	import("./pages/admin/denucias/SearchDenuncia")
+// );
+// const Denuncias = lazy(() => import("./pages/admin/denucias/Denuncias"));
+// const AdministrarUsuarios = lazy(() =>
+// 	import("./pages/admin/administrarUsuarios/AdministrarUsuarios")
+// );
 
-// Paginas Generales
-const NotFound = lazy(() => import("./pages/404"));
-const Tracking = lazy(() => import("./pages/public/Tracking/TrackingDenuncia"));
-const InfoDenuncia = lazy(() => import("./pages/public/Form/InfoDenuncia"));
-const Unauthorized = lazy(() => import("./pages/Unauthorized"));
-
-
+// // Paginas Generales
+// const NotFound = lazy(() => import("./pages/404"));
+// const Tracking = lazy(() => import("./pages/public/Tracking/TrackingDenuncia"));
+// const InfoDenuncia = lazy(() => import("./pages/public/Form/InfoDenuncia"));
+// const Unauthorized = lazy(() => import("./pages/Unauthorized"));
 
 function App() {
-
-    return (
-        <BrowserRouter>
-            <Suspense
-                fallback={
-                    <div className="flex min-h-screen w-full justify-center items-center h-screen">
-                        {/* <Loader  isBtn={true}/> */}
-                    </div>
-                }
-                >
-                
-                </Suspense>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Suspense
+				fallback={
+					<div className="flex min-h-screen w-full justify-center items-center h-screen">
+						<Loader />
+					</div>
+				}
+			>
+                <Routes>
+                    <Route path="/" element={<BaseLayout />}>
+						<Route index element={<FormDenuncia />} />
+					</Route>
+                </Routes>
+            </Suspense>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
