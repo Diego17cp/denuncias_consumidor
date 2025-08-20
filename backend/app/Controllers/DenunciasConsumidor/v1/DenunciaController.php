697f25df102ddc67d8144061bca81f6c8edb7709
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-19 20:17:17 -0500

    Remove enviarCorreoTracking method from DenunciaController to streamline code

--- backend/app/Controllers/DenunciasConsumidor/v1/DenunciaController.php
+++ backend/app/Controllers/DenunciasConsumidor/v1/DenunciaController.php
@@ -63,48 +63,4 @@ class DenunciaController extends ResourceController
             'data'    => $seguimientos
         ]);
     }
-
-    /**
-     * Simulación de envío de correo (aquí iría tu lógica real con email)
-     */
-    public function enviarCorreoTracking($correo, $code)
-    {
-        // Cargar la librería Email de CI4
-        $emailService = \Config\Services::email();
-
-        // Configuración del remitente y destinatario
-        $emailService->setFrom('anaga123op@gmail.com', 'AÑA');
-        $emailService->setTo($correo);
-        $emailService->setSubject('Código de Seguimiento de Denuncia');
-
-        // Mensaje en HTML con tu formato
-        $mensaje = "
-            <html>
-            <head>
-                <title>Código de Seguimiento de Denuncia</title>
-            </head>
-            <body style='font-family: Asap, sans-serif;'>
-                <p>Estimado usuario,</p>
-                <p>Su denuncia ha sido registrada exitosamente. A continuación, le proporcionamos su código de seguimiento:</p>
-                <p style='font-size: 18px; font-weight: bold; color: #2E8ACB; padding:15px; background-color: #CDDFEC;'>$code</p>
-                <p>Por favor, conserve este código para futuras consultas.</p>
-                <p>Para realizar el seguimiento de su denuncia, puede ingresar al siguiente enlace:</p>
-                <p><a href='http://localhost:5173/tracking-denuncia?codigo=$code'>Seguimiento</a></p>
-                <p>Atentamente,</p>
-                <p><strong>Municipalidad Distrital de José Leonardo Ortiz</strong></p>
-            </body>
-            </html>
-        ";
-
-        $emailService->setMessage($mensaje);
-
-        // Enviar correo y devolver resultado
-        if ($emailService->send()) {
-            log_message('info', "Correo enviado correctamente a {$correo}");
-            return true;
-        } else {
-            log_message('error', "Error al enviar correo: " . $emailService->printDebugger(['headers']));
-            return false;
-        }
-    }
 }
