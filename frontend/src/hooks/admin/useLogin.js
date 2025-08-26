d7e51a948f51bb3bbd16ba9b4c1dcc3289cebb35
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-26 12:43:46 -0500

    Add error toast for incorrect credentials in useLogin hook

--- frontend/src/hooks/admin/useLogin.js
+++ frontend/src/hooks/admin/useLogin.js
@@ -31,6 +31,7 @@ export const useLogin = () => {
 		try {
 			const success = await login(dni, password);
 			if (success) toast.success("Inicio de sesión exitoso");
+			else toast.error("Credenciales incorrectas");
 		} catch (err) {
 			if (axios.isAxiosError(err)) {
 				setError(err.response?.data?.error || "Error de red");
